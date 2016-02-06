a58a643fa65e39ddf6e1979dbc0d33ff94c9ee35
Romands312 <romands91@gmail.com>
2016-02-06 22:24:26 +0700

    Добавил модули

---
+++ Dimogir.DomainModel/Exercise.cs
@@ -0,0 +1,19 @@
+﻿namespace Dimogir.DomainModel
+{
+    public enum ExerciseType
+    {
+        Test,
+        FillText
+    }
+
+    public class Exercise
+    {
+        public string Id { get; set; }
+
+        public int Type { get; set; }
+
+        public string Answer { get; set; }
+
+        public string Description { get; set; }
+    }
+}
