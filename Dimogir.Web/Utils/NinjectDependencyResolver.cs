1d0206b113269247a51b693fc06a7979f662834b
Romands312 <romands91@gmail.com>
2016-02-14 23:16:10 +0700

    Залил NDR.cs

---
+++ Dimogir.Web/Utils/NinjectDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dimogir.Services;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace Dimogir.Web.Utils
+{
+    public class BlubbModule : GlobalKernelRegistrationModule<OnePerRequestHttpModule>
+    {
+    }
+
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Load(new BlubbModule());
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
+            kernel.Bind<ICategoryService>().To<CategoryService>();
+            kernel.Bind<ILessonService>().To<LessonService>();
+        }
+    }
+}
